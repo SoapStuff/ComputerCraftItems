96008ebd105541a4b77093b72bff401b2a52844f
DESKTOP-7QAVI7V\Stijn <s.f.swart@student.tudelft.nl>
2017-07-02 16:16:29 +0200

    Added the getAction method to turtle.

--- model/turtlebase.js
+++ model/turtlebase.js
@@ -44,6 +44,34 @@ module.exports.newTurtle = function (turtleId, callback) {
 };
 
 /**
+ * Gets the next action of the turtle with the specified id.
+ *
+ * @param id The id of the turtle
+ * @param callback The callback
+ */
+module.exports.getAction = function(id, callback) {
+    id = parseInt(id);
+    for (var i = 0; i < turtles.length; i++) {
+        if (turtles[i].id === id) {
+            const turtle = turtles[i];
+            logger.log("[Turtles] Returning next command of turtle with id " + id);
+            turtle.hasNextAction(function (hasNext) {
+                if (hasNext) {
+                    turtle.getNextAction(function (action) {
+                        logger.log("[Turtles] Returning action " + action + " to turtle with id " + id);
+                        callback(action);
+                    });
+                } else {
+                    logger.log("[Turtles] Turtle with id " + id + " has no next action");
+                    callback();
+                }
+            });
+            return;
+        }
+    }
+};
+
+/**
  * The turtle class.
  *
  * @param turtleId The id of the new turtle
@@ -65,8 +93,14 @@ function Turtle(turtleId, queue) {
         callback(action);
     };
 
-    this.hasNextAction = function () {
-        return !queue.isEmpty();
+    this.hasNextAction = function (callback) {
+        const hasNext = !queue.isEmpty();
+
+        if (callback !== undefined) {
+            callback(hasNext);
+        }
+
+        return hasNext;
     };
 
     return this;
