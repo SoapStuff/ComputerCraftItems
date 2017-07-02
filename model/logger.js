4ec02185360f1f69532f877ad347b50b376014e5
DESKTOP-7QAVI7V\Stijn <s.f.swart@student.tudelft.nl>
2017-07-02 15:37:50 +0200

    Worked on logger (yea sry wrong branch)

--- model/logger.js
+++ model/logger.js
@@ -1,7 +1,7 @@
 /**
  * Created by Stijn on 01/07/2017.
  */
-var logCommands, logRequests, logServer, logItembase, logTurtles;
+var logCommands, logRequests, logServer, logItembase, logTurtles, logCSV, logItemmap;
 
 module.exports.init = function (argument) {
     if (argument !== undefined) {
@@ -11,6 +11,8 @@ module.exports.init = function (argument) {
             logServer = false;
             logItembase = false;
             logTurtles = false;
+            logItemmap = false;
+            logCSV = false;
         }
         if (argument === 'turtle') {
             console.log("[Logger] started in turtle mode");
@@ -19,6 +21,17 @@ module.exports.init = function (argument) {
             logServer = false;
             logItembase = false;
             logTurtles = true;
+            logItemmap = false;
+            logCSV = false;
+        }
+        if (argument === 'true') {
+            logCommands = false;
+            logRequests = false;
+            logServer = false;
+            logItembase = false;
+            logTurtles = false;
+            logItemmap = false;
+            logCSV = false;
         }
     } else {
         logCommands = true;
@@ -26,6 +39,8 @@ module.exports.init = function (argument) {
         logServer = true;
         logItembase = true;
         logTurtles = true;
+        logItemmap = true;
+        logCSV = true;
     }
 };
 
@@ -35,6 +50,8 @@ module.exports.stopAll = function () {
     logServer = false;
     logItembase = false;
     logTurtles = false;
+    logItemmap = false;
+    logCSV = false;
 };
 
 module.exports.log = function (string) {
@@ -51,6 +68,12 @@ module.exports.log = function (string) {
         return;
     }
     if (!logTurtles && string.indexOf('Turtles') !== -1) {
+        return;
+    }
+    if (!logItemmap && string.indexOf('ItemMap') !== -1) {
+        return;
+    }
+    if (!logCSV && string.indexOf('CSV-Parser') !== -1) {
         return;
     }
     console.log(string);
