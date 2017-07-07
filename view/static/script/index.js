cfd53c05cbbd0897934160622e7d2705f1b2563f
DESKTOP-7QAVI7V\Stijn <s.f.swart@student.tudelft.nl>
2017-07-07 02:03:41 +0200

    Made turtlelist scrollable

--- view/static/script/index.js
+++ view/static/script/index.js
@@ -1,6 +1,11 @@
 /**
  * Created by Stijn on 03/07/2017.
  */
+var turtles;
+var firstIndex;
+var list;
+var selected;
+
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -30,47 +35,99 @@ function setPage(page) {
 }
 
 function selectTurtle(img) {
-    var source = img.src;
-
     var id = img.id;
     var imgList = document.getElementsByTagName("img");
-    console.log(imgList);
     for (var i = 0; i < imgList.length; i++) {
         var image = imgList[i];
 
-        console.log(image);
         if (image.src.indexOf('Slot') !== -1) {
             if (image.id === id) {
                 image.src = "static/imgs/selectedSlot.png";
+                selected = id;
             } else {
                 image.src = "static/imgs/unselectedSlot.png";
             }
         }
     }
-
 }
 
-var firstThree = [];
-var turtles;
-
 function showTurtles() {
-    for (var i = 0; i = firstThree; i++) {
-        console.log(firstThree[i]);
-    }
+    for (var i = 0; i < 3; i++) {
+        var index = i + firstIndex;
 
-    for (var i = 0; i < turtles.length; i++) {
-        if (firstThree.indexOf(turtles[i]) !== -1) {
-            turtles[i].className = turtles[i].className.replace("hidden","");
+        if (index >= turtles.length) {
+            index -= turtles.length;
+        }
+
+        if (selected !== undefined && selected === turtles[index]) {
+            list.append(
+                "<li id='" + turtles[index] + "' >" +
+                "    <img id='" + turtles[index] + "' class='background' onclick='selectTurtle(this)' src='static/imgs/selectedSlot.png'> " +
+                "    <img id='" + turtles[index] + "' class='foreground' onclick='selectTurtle(this)' src='static/imgs/turtle.png'> " +
+                "</li>"
+            )
         } else {
-            turtles[i].className += " hidden";
+            list.append(
+                "<li id='" + turtles[index] + "' >" +
+                "    <img id='" + turtles[index] + "' class='background' onclick='selectTurtle(this)' src='static/imgs/unselectedSlot.png'> " +
+                "    <img id='" + turtles[index] + "' class='foreground' onclick='selectTurtle(this)' src='static/imgs/turtle.png'> " +
+                "</li>"
+            )
         }
     }
+
+    list.append(
+        "<li id='down-scroll'><img class='down-scroll' onclick='scroll()' src='static/imgs/TempScrollArrow.png'></li>"
+    )
+}
+
+function removeTurtles(callback) {
+    turtles.forEach(function (id) {
+        var turtle = $('#' + id)[0];
+
+        if (turtle !== undefined) {
+            turtle.parentNode.removeChild(turtle);
+        }
+    });
+
+    var scroll = $('#down-scroll')[0];
+
+    if (scroll !== undefined) {
+        scroll.parentNode.removeChild(scroll);
+    }
+
+    callback();
+}
+
+function scroll(direction) {
+    var index = firstIndex;
+
+    if (direction !== undefined && direction === 'up') {
+        index++;
+
+        if (index === turtles.length) {
+            index = 0;
+        }
+    } else {
+        index--;
+
+        if (index === -1) {
+            index = turtles.length - 1;
+        }
+    }
+
+    firstIndex = index;
+
+    removeTurtles(function () {
+        showTurtles();
+    });
 }
 
 $(document).ready(function () {
-    turtles = document.getElementsByClassName('hidden');
-    firstThree[0] = turtles[0];
-    firstThree[1] = turtles[1];
-    firstThree[2] = turtles[2];
-    showTurtles();
+    $.get('/getTurtleIDList').done(function (data) {
+        turtles = data.split(",");
+        list = $("#turtle-container");
+        firstIndex = 0;
+        showTurtles();
+    });
 });
